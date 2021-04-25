77f892576cfd554cddaab60dc5b61d6740e94c33
omegazitro <morti137@stu.southtexascollege.edu>
2021-04-25 17:41:17 -0500

    Fixed useAuth

--- src/helpers/useAuth.js
+++ src/helpers/useAuth.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import { firebaseConfig } from '../config/firebase'
 import { useAuth } from '@vueuse/firebase'
+import 'firebase/firestore'
 
 firebase.initializeApp(firebaseConfig)
 
